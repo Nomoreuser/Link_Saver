740a528b075cd0382194c47ad4d6503a31542ddb
Gerald <geraldacarvajal@gmail.com>
2026-06-04 02:46:27 +0800

    Add input

--- src/script.js
+++ src/script.js
@@ -1,7 +1,32 @@
 
-// document.body.addEventListener("click", (e)=>{
+// const textarea = document.getElementById('inputc');
 
-//     if(e.button === 2){
-//         alert("left click")
-//     }
-// })
+// window.addEventListener('keypress', ()=>{
+//     textarea.focus()
+// });
+
+
+const input = document.getElementById('hinput');
+const pvalue = document.getElementById('text');
+const cursor = document.getElementById('cursor');
+
+window.addEventListener('keypress', ()=>{
+    input.focus();
+})
+
+
+let isTyping;
+cursor.classList.add("blink")
+input.addEventListener('input', ()=>{
+    cursor.classList.remove("blink")
+    clearTimeout(isTyping);
+
+    pvalue.textContent = input.value;
+
+    isTyping = setTimeout(()=>{
+        cursor.classList.add("blink")
+    },1000)
+});
+
+
+
